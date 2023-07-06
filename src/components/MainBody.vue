0800b886d4f780e0d2f72ef6d6f66baa1c9b5758
VenkataSuresh7989 <mendavenkatasuresh@gmail.com>
2023-07-06 15:59:43 +0530

    image updated

--- src/components/MainBody.vue
+++ src/components/MainBody.vue
@@ -3,7 +3,7 @@
 		<div class="banner-container">
 			<b-carousel id="carousel-1" v-model="slide" :interval="4000" fade controls indicators background="#ababab" img-width="1024" img-height="480" style="text-shadow: 1px 1px 2px #333;" @sliding-start="onSlideStart" @sliding-end="onSlideEnd">
 				<b-carousel-slide caption="First slide" text="Nulla vitae elit libero, a pharetra augue mollis interdum." img-src="https://picsum.photos/1024/480/?image=52"></b-carousel-slide>
-				<b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
+				<b-carousel-slide img-src="https://i.postimg.cc/c1Q98HBW/tiger.jpg">
 					<h1>Hello world!</h1> 
                 </b-carousel-slide>
 				<b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
